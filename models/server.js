b5efc6afc845306d46c690f4cd9d03f3299a6f97
Benji Mtz <benjim.flores@outlook.com>
2021-03-26 01:08:17 -0600

    add cors

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
-
+const cors = require('cors'); 
 
 class Server {
 
@@ -26,6 +26,8 @@ class Server {
         // Desplegar el directorio publico
         this.app.use( express.static( path.resolve( __dirname , '../public')) );
 
+        // Habilitar Cors
+        this.app.use( cors() );
     }
 
     configurarSockets() {
